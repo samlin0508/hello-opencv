1e9d454754b3e607bb50f7107c396997b052fd9b
Sam Lin <samlin0508@gmail.com>
2024-04-30 06:09:39 +0000

    first commit

---
+++ hello-opencv.py
@@ -0,0 +1,9 @@
+import cv2 as cv
+import pytesseract as pytesseract
+
+avatar = cv.imread("./line_avatar_yellow_480.png")
+avatar_in_gray = cv.cvtColor(avatar, cv.COLOR_BGR2GRAY)
+avatar_in_gaussian_blur = cv.GaussianBlur(avatar_in_gray, (5,5), 0)
+avatar_in_canny = cv.Canny(avatar_in_gaussian_blur, 100, 150)
+captured_text = pytesseract.image_to_string(avatar_in_canny, lang='eng')
+print(captured_text)
