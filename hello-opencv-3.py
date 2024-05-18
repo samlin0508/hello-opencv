7ab4666d372e34376a903da10fa8399e2932e9a7
Sam Lin <samlin0508@gmail.com>
2024-05-18 02:02:30 +0000

    try pytesseract.image_to_boxes.

--- hello-opencv-3.py
+++ hello-opencv-3.py
@@ -16,5 +16,12 @@ cv.imwrite('output/avatar_in_canny.jpg', avatar_in_canny)
 cv.imwrite('output/thresholded_image.jpg', thresholded_image)
 
 captured_text = pytesseract.image_to_string(avatar_in_gray, lang='chi_tra')
+boxes = pytesseract.image_to_boxes(avatar_in_gray)
+
+for line in boxes.splitlines():
+    axis = line.split(' ')
+    x, y, w, h = int(axis[1]), int(axis[2]), int(axis[3]), int(axis[4])
+    cv.rectangle(avatar, (x, avatar.shape[0] - y), (w, avatar.shape[0] - h), (0, 0, 255), 2)
 
 print(captured_text)
+cv.imwrite('output/test6-rounded.jpg', avatar)
